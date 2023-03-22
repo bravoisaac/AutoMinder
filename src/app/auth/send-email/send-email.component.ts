51df642ba6db6f269aff833793cdfcf4f5681de1
joherrerac <jo.herrerac@duocuc.cl>
2023-03-22 12:06:34 -0300

    termino funcionalidad validación email

--- src/app/auth/send-email/send-email.component.ts
+++ src/app/auth/send-email/send-email.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 
 @Component({
   selector: 'app-send-email',
   templateUrl: './send-email.component.html',
   styleUrls: ['./send-email.component.scss'],
-  providers: [AuthService]
+  providers: [AuthenticationService]
 })
 export class SendEmailComponent implements OnInit {
   public user$:Observable<any>= this.authSvc.afAuth.user
   
   constructor(private authSvc: AuthService) { }
 
-  ngOnInit():  void{}
+  ngOnInit():  void{
+}
+
+  onSendEmail(): void{
+    this.authSvc.sendVerificationEmail();
+  }
 
 }
--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import {
   Auth,
   signInWithEmailAndPassword,
@@ -10,7 +11,11 @@ import {
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor(private auth: Auth) {}
+  constructor(private auth: Auth, private router: Router) {}
+
+  async sendVerificationEmail(): Promise<void> {
+    return (await this.afAuth.currentUser).sendEmailVerification();
+  }
 
   // async register({ email, password }) {
   //   try {
@@ -25,10 +30,11 @@ export class AuthenticationService {
   //     return null;
   //   }
   // }
-  register({ email, password }) {
+  async register({ email, password }) {
     createUserWithEmailAndPassword(this.auth, email, password).then(
       (userCredential) => {
         const user = userCredential.user;
+        this.sendVerificationEmail();
         console.log(user);
       }
     );
@@ -36,6 +42,13 @@ export class AuthenticationService {
   async login({ email, password }) {
     try {
       const user = await signInWithEmailAndPassword(this.auth, email, password);
+      if (user && user.user.emailVerified){
+        this.router.navigate(['/home']);
+      } else if (user){
+        this.router.navigate(['/verification-email']);
+      } else {
+        this.router.navigate(['/register']);
+      }
       return user;
     } catch (error) {
       console.log(error);
